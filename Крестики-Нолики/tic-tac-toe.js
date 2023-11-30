172f854a416b485c36adeff15c5deec1f9e0d82e
thisStorageIsMine <133993782+thisStorageIsMine@users.noreply.github.com>
2023-11-30 12:44:38 +0300

    ну всё
    Ну всё

--- Крестики-Нолики/tic-tac-toe.js
+++ Крестики-Нолики/tic-tac-toe.js
@@ -3,20 +3,8 @@ const table = document.querySelector("table"),
       title = document.querySelector("#title"),
       btn = document.querySelector("button");
       dialog = document.querySelector("#dialog");
-let player = true;
-const winbos  = [
-    "012",
-    "345",
-    "678", // горизонтальные все
-
-    "036",
-    "147",
-    "258", // Вертикальные все
-
-    "048",
-    "246"
-];
-let lastWords;
+let player = true,
+    lastWords;
 
 
 function Modal(text) {
@@ -27,31 +15,18 @@ function Modal(text) {
 
 
 // Эта штука нужна чтобы проверить, кто победил
-function isSubstringInWord(frozenWord, word) {
-    // Отсортировать frozenWord
-    const sortedFrozenWord = frozenWord.split('').sort().join('');
-  
-    // Инициализировать окно скользящего метода
-    const windowSize = frozenWord.length;
-    let window = word.split('').sort().join('').slice(0, windowSize);
-  
-    // Проверка первого окна
-    if (window === sortedFrozenWord) {
-      return true;
+function isSubstringInWord(arr) {
+    if ((arr.includes("0") & arr.includes("4") & arr.includes("8")) 
+        || (arr.includes("2") & arr.includes("4") & arr.includes("6"))
+        || arr.includes("0") & arr.includes("1") & arr.includes("2")
+        || (arr.includes("3") & arr.includes("4") & arr.includes("5"))
+        || (arr.includes("6") & arr.includes("7") & arr.includes("8"))
+        || arr.includes("0") & arr.includes("3") & arr.includes("6")
+        || (arr.includes("1") & arr.includes("4") & arr.includes("7"))
+        || (arr.includes("2") & arr.includes("5") & arr.includes("8"))) {
+            return true;
     }
-  
-    // Перебор оставшихся символов в word
-    for (let i = windowSize; i < word.length; i++) {
-      // Сдвинуть окно, добавив новый символ и удалив старый
-      window = window.slice(1) + word[i];
-  
-      // Проверка текущего окна
-      if (window === sortedFrozenWord) {
-        return true;
-      }
-    }
-  
-    // Подстрока не найдена
+
     return false;
 }
 
@@ -75,23 +50,23 @@ const checkIfSomebodyWin = ()  => {
     });
 
 
-    for(let item of winbos) {
+    // for(let item of winbos) {
         
-        if(isSubstringInWord(item,crossArr) || (crossArr.includes("0") & crossArr.includes("4") & crossArr.includes("8")) || (crossArr.includes("2") & crossArr.includes("4") & crossArr.includes("6"))) {
-            lastWords = "Победил крестик!";
-            Modal(lastWords);
-            break;
-        } else if(isSubstringInWord(item,circleArr) || (circleArr.includes("2") & circleArr.includes("4") & circleArr.includes("6")) || (circleArr.includes("0") & circleArr.includes("4") & circleArr.includes("8"))){
-            lastWords = "Победил нолик!";
-            Modal(lastWords);
-            break;
-        } else if(crossArr.length === 5 || circleArr.length ===5){
-            lastWords = "Ничья!";
-            Modal(lastWords);
-            break;
-        }
+    if(isSubstringInWord(crossArr)) {
+        lastWords = "Победил крестик!";
+        Modal(lastWords);
+        // break;
+    } else if(isSubstringInWord(circleArr)){
+        lastWords = "Победил нолик!";
+        Modal(lastWords);
+        // break;
+    } else if(crossArr.length === 5 || circleArr.length ===5){
+        lastWords = "Ничья!";
+        Modal(lastWords);
+        // break;
     }
-}
+    }
+// }
 
 
 btn.addEventListener("click", ()=>{
